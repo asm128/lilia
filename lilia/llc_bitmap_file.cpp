2bc3644f04048aa2481de8e3a844f64a3108d87a
asm128 <pabloarielz@outlook.com>
2018-04-23 10:12:48 -0300

    -

--- lilia/llc_bitmap_file.cpp
+++ lilia/llc_bitmap_file.cpp
@@ -93,6 +93,53 @@ struct SHeaderInfoBMP {
 	return 0;
 }
 
+					BOOL																			LoadBitmapFromBMPFile						(LPCSTR szFileName, HBITMAP *phBitmap, HPALETTE *phPalette)		{
+	BITMAP																									bm;
+	*phBitmap																							= NULL;
+	*phPalette																							= NULL;
+	// Use LoadImage() to get the image loaded into a DIBSection
+	*phBitmap																							= (HBITMAP)LoadImageA( NULL, szFileName, IMAGE_BITMAP, 0, 0, LR_CREATEDIBSECTION | LR_DEFAULTSIZE | LR_LOADFROMFILE );
+	if( *phBitmap == NULL )
+	  return FALSE;
+
+	// Get the color depth of the DIBSection
+	GetObject(*phBitmap, sizeof(BITMAP), &bm );
+	// If the DIBSection is 256 color or less, it has a color table
+	if((bm.bmBitsPixel * bm.bmPlanes) <= 8) {
+		HDC																										hMemDC;
+		HBITMAP																									hOldBitmap;
+		RGBQUAD																									rgb[256];
+		LPLOGPALETTE																							pLogPal;
+		// Create a memory DC and select the DIBSection into it
+		hMemDC																								= CreateCompatibleDC(NULL);
+		hOldBitmap																							= (HBITMAP)SelectObject(hMemDC, *phBitmap);
+		// Get the DIBSection's color table
+		GetDIBColorTable(hMemDC, 0, 256, rgb);
+		// Create a palette from the color tabl
+		pLogPal																								= (LOGPALETTE *)malloc(sizeof(LOGPALETTE) + (256*sizeof(PALETTEENTRY)));
+		pLogPal->palVersion																					= 0x300;
+		pLogPal->palNumEntries																				= 256;
+		for(WORD i = 0; i < 256; ++i) {
+		  pLogPal->palPalEntry[i].peRed																			= rgb[i].rgbRed;
+		  pLogPal->palPalEntry[i].peGreen																		= rgb[i].rgbGreen;
+		  pLogPal->palPalEntry[i].peBlue																		= rgb[i].rgbBlue;
+		  pLogPal->palPalEntry[i].peFlags																		= 0;
+		}
+		*phPalette																							= CreatePalette( pLogPal );
+		// Clean up
+		free( pLogPal );
+		SelectObject(hMemDC, hOldBitmap);
+		DeleteDC	(hMemDC);
+	}
+	else { // It has no color table, so use a halftone palette
+		HDC																										hRefDC;
+		hRefDC																								= GetDC( NULL );
+		*phPalette																							= CreateHalftonePalette( hRefDC );
+		ReleaseDC(NULL, hRefDC);
+	}
+	return TRUE;
+}
+
 // Currently supporting only 24-bit bitmaps
 					::llc::error_t																	llc::bmpFileLoad							(FILE* source, ::llc::array_pod<::llc::SColorBGRA>& out_Colors, ::llc::grid_view<::llc::SColorBGRA>& out_ImageView)					{
 	::SHeaderFileBMP																						fileHeader									= {};	
